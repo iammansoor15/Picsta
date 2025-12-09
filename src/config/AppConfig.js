8944a4e861ce9bd20d65e3569b4ac41b9e84d9e5
iammansoor15 <eesamansoorpatan@gmail.com>
2025-12-10 03:14:19 +0530

    version 6

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -3,7 +3,7 @@ const AppConfig = {
   PRODUCTION_AUTH_SERVER_URL:  'https://codebinary.in',
   
   APP_NAME: 'Winter',
-  VERSION: '1.0.5',
+  VERSION: '1.0.6',
   
   BACKGROUND_REMOVAL: {
     MAX_IMAGES_PER_BATCH: 10,
