42a43bd925a37f9dbbb8be9878d8bacceb3b5523
iammansoor15 <eesamansoorpatan@gmail.com>
2025-12-10 05:21:59 +0530

    version 8

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,9 +1,9 @@
 const AppConfig = {
-  PRODUCTION_SERVER_URL: 'http://192.168.1.76:10000',
-  PRODUCTION_AUTH_SERVER_URL:  'http://192.168.1.76:10000',
+  PRODUCTION_SERVER_URL: 'https://codebinary.in',
+  PRODUCTION_AUTH_SERVER_URL:  'https://codebinary.in',
   
   APP_NAME: 'Winter',
-  VERSION: '1.0.6',
+  VERSION: '1.0.8',
   
   BACKGROUND_REMOVAL: {
     MAX_IMAGES_PER_BATCH: 10,
@@ -15,8 +15,8 @@ const AppConfig = {
     ENABLE_LOGGING: true,
     SHOW_SERVER_CONFIG: true, 
 
-    DEV_SERVER_URL: 'http://192.168.1.76:10000',
-    DEV_AUTH_SERVER_URL: 'http://192.168.1.76:10000',
+    DEV_SERVER_URL: 'https://codebinary.in',
+    DEV_AUTH_SERVER_URL: 'https://codebinary.in',
 
     USE_PRODUCTION_FALLBACK: true, 
   }
